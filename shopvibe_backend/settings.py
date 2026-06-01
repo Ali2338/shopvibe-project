959b1d7d7917a070fc0e22b94053953ad9c23c76
Ali2338 <mohammadali98999899@gmail.com>
2026-06-01 20:02:35 +0530

    update database for deployment

--- shopvibe_backend/settings.py
+++ shopvibe_backend/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from datetime import timedelta
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 # Load environment variables from the .env file
 load_dotenv()
@@ -79,15 +80,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'shopvibe_backend.wsgi.application'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Database
+# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 
