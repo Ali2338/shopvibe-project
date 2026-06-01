ae0ac64b86a77e8ed990aee745a95887863fab5b
Ali2338 <mohammadali98999899@gmail.com>
2026-06-01 21:36:39 +0530

    make settings changes

--- shopvibe_backend/settings.py
+++ shopvibe_backend/settings.py
@@ -29,9 +29,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'shopvibe-project.onrender.com', # CORRECT
+    'localhost',
+    '127.0.0.1'
+]
 
 
 # Application definition
